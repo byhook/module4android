376fc93c1385b46040098d4db9c76275956905f8
byhook <byhook@163.com>
2020-06-09 14:14:42 +0800

    初步加入网络模块

---
+++ module-network/src/main/java/com/handy/module/core/NetCore.java
@@ -0,0 +1,20 @@
+package com.handy.module.core;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import okhttp3.OkHttpClient;
+
+/**
+ * @author: handy
+ * @date: 2020-06-09
+ * @description:
+ */
+public class NetCore {
+
+    public void httpPost(){
+
+    }
+
+}
